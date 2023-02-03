2f41eafd74f8d8192bababff132875af259963c6
Lionard Leyva <lleyvah@upao.edu.pe>
2023-02-03 03:51:25 +0000

    Initial commit

---
+++ src/main/java/com/electronic/invoice/Application.java
@@ -0,0 +1,18 @@
+package com.electronic.invoice;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "electronic-invoice-api",
+            version = "0.0"
+    )
+)
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
